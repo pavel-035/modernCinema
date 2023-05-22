be610d028b22d896f009a26402a74d0628678a66
pavel <kdpavel8@gmail.com>
2023-05-22 18:20:14 +0500

    карточка фильма

---
+++ pages/_movie/_muvie.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="movie">
+    <b-tabs content-class="mt-3" fill>
+      <b-tab title="О фильме" active>
+        <movie-card class="w-75 ml-auto mr-auto"/>
+      </b-tab>
+      <b-tab title="Сеансы"><p>I'm the second tab</p></b-tab>
+    </b-tabs>
+  </div>
+</template>
+
+<script>
+import movieCard from '~/components/movie/card';
+export default {
+  layout: 'main',
+  components: { movieCard }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

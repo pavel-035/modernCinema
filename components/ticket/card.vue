691bced5959b816639a51e8fcc4602a69d6e491d
pavel <kdpavel8@gmail.com>
2023-05-30 19:28:23 +0500

    tickets

---
+++ components/ticket/card.vue
@@ -0,0 +1,48 @@
+<template>
+  <b-card>
+    <b-row>
+      <b-col>
+        <div class="mb-3">
+          <div class="mb-1">Фильм</div>
+          <b>Название фильма</b>
+        </div>
+        <div class="mb-3">
+          <div class="mb-1">Кинотеатр</div>
+          <b>Наименование кинотеатра</b>
+        </div>
+        <div class="mb-3">
+          <div class="mb-1">Дата сеанса</div>
+          <b>Дата и время сеанса</b>
+        </div>
+        <div class="mb-3">
+          <div class="mb-1">Зал</div>
+          <b>Номер зала</b>
+        </div>
+        <div class="mb-3">
+          <div class="mb-1">Ряд и место</div>
+          <b>Ряд и места</b>
+        </div>
+        <div class="mb-3">
+          <div class="mb-1">Цена</div>
+          <b>Суммарная стоимость</b>
+        </div>
+      </b-col>
+      <b-col>
+        <div class="mb-3">
+          <div class="mb-1">Номер заказа</div>
+          <b>Номер заказа</b>
+        </div>
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/user/tickets.vue
+++ pages/user/tickets.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    tickets
+    <ticket-card/>
   </div>
 </template>
 
 <script>
+import ticketCard from '~/components/ticket/card';
 export default {
-  name: 'tickets'
+  name: 'tickets',
+  components: { ticketCard }
 
 }
 </script>
